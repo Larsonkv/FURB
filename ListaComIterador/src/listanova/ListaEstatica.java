3bab88171beb7d24cbe7e283a1b0f5185da9f0ae
Larsonkv <50681729+Larsonkv@users.noreply.github.com>
2021-08-31 21:02:12 -0300

    att

--- ListaComIterador/src/listanova/ListaEstatica.java
+++ ListaComIterador/src/listanova/ListaEstatica.java
@@ -131,4 +131,16 @@ public class ListaEstatica <ClasseInformadaProgramador> implements Iterator{
 			return info[backup];
 		}
 	}
+
+	@Override
+	public boolean hasNext() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Object next() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
